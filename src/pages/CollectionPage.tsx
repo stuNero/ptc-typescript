83c710b5d57183d0520bbe6dcd45a059953bdb3d
stuNero <max.vemic@student.nbi-handelsakademin.se>
2026-01-27 09:36:47 +0100

    change import of data to useEffect + restructuring

--- src/pages/CollectionPage.tsx
+++ src/pages/CollectionPage.tsx
@@ -1,28 +1,53 @@
 import Card from "../Card";
-import useFetchJson from "../utils/useFetchJson";
+import type { CardData } from "../Card";
+import { useState } from 'react';
+import Select from '../Select';
+import { useEffect } from "react";
 
-let filterWordRaw = "fire";
-let filterWord = filterWordRaw.slice(0, 1).toUpperCase() + filterWordRaw.slice(1);
-
-interface cardImage {
-  small: string;
-  large: string;
-}
-export interface CardData {
-  name: string;
-  price: number;
-  stock: number;
-  types: string[];
-  images: cardImage;
-}
 export default function CollectionPage() {
-  const cards = useFetchJson<CardData[]>("/data1-1.json");
+  const [filterType, setFilterType] = useState('All');
+  const [limit, setLimit] = useState<number | undefined>(10);
+  const [cards, setCards] = useState<CardData[]>([]);
+
+  async function getPokemonCardsData() {
+    const response = await fetch("/data1-1.json");
+    const result = await response.json();
+
+    setCards(result);
+  }
+
+  useEffect(() => {
+    getPokemonCardsData();
+  }, []);
+
+  const cardTypesSet = new Set(
+    cards
+      ?.map((card) => card.types)
+      .flat()
+      .filter(type => type !== '' && type)
+  );
+  const cardTypes = Array.from(cardTypesSet);
+
   return cards && <>
     <h1>Pokemon kort: </h1>
-    <div>
+    <div id="selections">
+      <Select {...{
+        label: "Type",
+        values: ['All'].concat(cardTypes),
+        setter: setFilterType
+      }} />
+      <label htmlFor="limit-input">Limit of max length: {cards.length}:</label>
+      <input
+        type="number"
+        id="limit-input"
+        value={limit || ''}
+        onChange={(e) => setLimit(e.target.value ? parseInt(e.target.value) : undefined)}
+      ></input>
+    </div >
+    <div id="cards-grid">
       {cards
-        .filter(({ types }) => types?.includes(filterWord))
-        ?.map((props, i) => <Card key={i} {...props} />)
+        .filter(({ types }) => filterType === 'All' || types?.includes(filterType))
+        .slice(0, limit)?.map((props, i) => <Card key={i} {...props} />)
       }
     </div>
   </>;
