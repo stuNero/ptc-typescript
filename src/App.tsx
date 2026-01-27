5a26c513474248b541a0bf5e08dfa0a662976d5d
stuNero <max.vemic@student.nbi-handelsakademin.se>
2026-01-27 09:41:03 +0100

    add header and footer to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useLocation } from 'react-router-dom';
-import Card from "./Card";
+import Header from './partials/Header';
 import Main from "./partials/Main";
+import Footer from './partials/Footer';
 
 
 export default function App() {
   useLocation();
   window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
   return <>
+    <Header />
     <Main />
+    <Footer />
   </>;
 };
